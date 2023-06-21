d99a0197c4aee20b64c891a41a82cbce9c113028
sh2050kevin <40140645+sh2050kevin@users.noreply.github.com>
2023-06-22 01:55:11 +0800

    新增登录页面

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import Vant from 'vant'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './styles/index.css'
 import 'vant/lib/index.css'
+import './styles/index.less'
 import 'amfe-flexible'
 
 Vue.use(Vant)
---
+++ src/views/login/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="login-container">
+    <van-nav-bar title="登录" class="page-nav-bar"/>
+    <van-form @submit="onSubmit">
+      <van-cell-group inset>
+        <van-field
+          name="手机号"
+          placeholder="请输入手机号"
+        >
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+        <van-field
+          type="password"
+          name="验证码"
+          placeholder="请输入验证码"
+        >
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <van-button size="small" round class="send-sms-btn" type="primary">获取验证码</van-button>
+        </template>
+      </van-field>
+      </van-cell-group>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" type="primary" native-type="submit">
+          登录
+        </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+
+  data () {
+    return {
+    }
+  },
+
+  mounted () {
+  },
+
+  methods: {
+    onSubmit () {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .login-container {
+    .toutiao-shouji,.toutiao-yanzhengma {
+        font-size: 37px;
+        margin-right: 39px;
+    }
+    .send-sms-btn {
+      background-color: #ededed;
+      border: none;
+      height: 46px;
+      color: #666;
+    }
+    .login-btn-wrap {
+      padding: 53px 28px;
+      .login-btn {
+        background-color: #6db4fb;
+        width: 694px;
+        height: 88px;
+        border: none;
+        color: #fff;
+        font-size: 30px;
+      }
+    }
+  }
+</style>
